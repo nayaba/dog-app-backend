ce51db071d7972b461f02c31a16627c10d94788f
Nabila Ayaba <nabila.ayaba@gmail.com>
2023-05-15 11:19:07 -0400

    Create routes directory

--- server.js
+++ server.js
@@ -1,33 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const dogsRouter = require('./routes/dogs')
 
-// CRUD ROUTES
-
-// GET all dogs
-app.get('/dogs', (req, res) => {
-  res.send('Hello World!')
-})
-
-// POST create a dog
-app.post('/dogs', (req, res) => {
-  res.send('This is the POST route for dogs')
-})
-
-// GET one dog (SHOW)
-app.get('/dogs/:id', (req, res) => {
-  res.send('This is the GET route for one dog')
-})
-
-// PUT one dog (UPDATE)
-app.put('/dogs/:id', (req, res) => {
-  res.send('This is the PUT route for one dog')
-})
-
-// DELETE one dog
-app.delete('/dogs/:id', (req, res) => {
-  res.send('This is the DELETE route for one dog')
-})
+// ROUTES
+app.use('/dogs', dogsRouter)
 
 app.listen(port, () => {
   console.log('You are connected to the Port 3000')
