ca1c4985d184bd201543ad9525c627dc9eea32d6
Nabila Ayaba <nabila.ayaba@gmail.com>
2023-05-15 10:55:02 -0400

    Create CRUD routes

--- server.js
+++ server.js
@@ -2,8 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+// CRUD ROUTES
+
+// GET all dogs
+app.get('/dogs', (req, res) => {
   res.send('Hello World!')
+})
+
+// POST create a dog
+app.post('/dogs', (req, res) => {
+  res.send('This is the POST route for dogs')
+})
+
+// GET one dog (SHOW)
+app.get('/dogs/:id', (req, res) => {
+  res.send('This is the GET route for one dog')
+})
+
+// PUT one dog (UPDATE)
+app.put('/dogs/:id', (req, res) => {
+  res.send('This is the PUT route for one dog')
+})
+
+// DELETE one dog
+app.delete('/dogs/:id', (req, res) => {
+  res.send('This is the DELETE route for one dog')
 })
 
 app.listen(port, () => {
